2118dc66e29e634b11d7e5d7b4888e11c07d90fc
wangbiaozhi <null>
2017-02-20 22:20:59 +0800

    增加了简单留言功能

---
+++ php/updateArticle.sql
@@ -0,0 +1,1 @@
+insert article(id,type,imgSrc,title,abstract ,author ,date,number) values(1,'AJAX','同步和异步的区别?','./images/article_1.jpg','同步：阻塞的，比如：-张三叫李四去吃饭，李四一直忙得不停，张三一直等着，直到李四忙完两个人一块去撒大大三大阿发顺丰.','Mr Wang','大萨达四大','20');
